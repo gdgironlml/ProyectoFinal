a08f7e6fd8b24bb379e3308e86c956714f90f203
Daniel Girón <ggironc1@miumg.edu.gt>
2026-05-29 21:33:47 -0600

    Incorporación del Informe de Participación por Integrante.docx

--- SuperBodega.API/obj/Debug/net10.0/SuperBodega.API.AssemblyInfo.cs
+++ SuperBodega.API/obj/Debug/net10.0/SuperBodega.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperBodega.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b23727f63f8aeec4ae9a7e612ab03e7acba690d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41cfe4ea91eb6541966c90325cbfaed870fac558")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperBodega.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperBodega.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
